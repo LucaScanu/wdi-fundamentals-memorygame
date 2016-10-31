fef0a6a6e3828d294efc9032f0139e14d5dbf96f
Gianluca Scanu <Luca@Gianlucas-MacBook-Pro.local>
2016-10-31 16:12:46 +0000

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,9 @@
 console.log("JS file is connected to HTML! Woo!")
+
+/* cards created for the meomry game */
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
