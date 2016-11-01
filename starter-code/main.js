ce38c63dee67e0146b089b486e351db559fd33e4
Gianluca Scanu <Luca@Gianlucas-MacBook-Pro.local>
2016-11-01 18:04:09 +0000

    created game logic/2

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,16 @@
 console.log("JS file is connected to HTML! Woo!")
 
-/* cards created for the meomry game */
+/* created cards for the memory game */
 
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+/* Game logic */
+
+if (cardTwo === cardFour) {
+	alert("You have found a match!");
+} else {
+	alert("Sorry, try again!");
+}
