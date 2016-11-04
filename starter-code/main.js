81028b74898360aa9ef58edb4bd02997702f956d
Gianluca Scanu <Luca@Gianlucas-MBP.homenet.telecomitalia.it>
2016-11-04 15:57:34 +0100

    created game board with JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -9,8 +9,27 @@ var cardFour = "king";
 
 /* Game logic */
 
-if (cardTwo === cardFour) {
+/* if (cardTwo === cardFour) {
 	alert("You have found a match!");
 } else {
 	alert("Sorry, try again!");
 }
+document.createElement('div'); */
+
+//creating cards with JavaScript
+
+var createCards = function() {
+
+var mainBoard = document.getElementById('game-board');
+
+for (var i = 0; i < 4; i++) {
+	var playingCards = document.createElement('div'); //created a new div
+	playingCards.className = 'card'; //given a class to the new div
+
+	mainBoard.appendChild(playingCards); //added new div to game board
+
+}
+};
+
+createCards(); //calling the function
+
